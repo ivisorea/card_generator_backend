280989ab0bb5f3313816c4cd89e8b21b28ea4a7d
Ivis Orea <ivisorea@Iviss-MBP.fritz.box>
2021-12-13 14:10:31 +0100

    some text

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ app.use(express.json());
 app.use('/images', imagesRouter)
 app.use('/messages', messageRoutes)
 app.use('*', (req, res) => res.send(
-    `API <br/> <p>https://cardgeneratordiwali.herokuapp.com/images/:id </p>
-    <p>https://cardgeneratordiwali.herokuapp.com/messages/:id </p>
-     `));
+    `<h1>API</h1> <br/> 
+    <h2>Endpoints</h2>
+    <ul>
+        <li>To get all the images <a href="https://cardgeneratordiwali.herokuapp.com/images">https://cardgeneratordiwali.herokuapp.com/images</a></li>
+        <li>To get all messages <a href="https://cardgeneratordiwali.herokuapp.com/messages">https://cardgeneratordiwali.herokuapp.com/messages</a></li>
+        <li>To get the images by id <b>https://cardgeneratordiwali.herokuapp.com/images/:id</b></li>
+        <li>To get the messages by id <b>https://cardgeneratordiwali.herokuapp.com/messages/:id</b></li>
+    </ul>`
+))
+
 
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
